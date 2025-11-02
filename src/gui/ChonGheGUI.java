3395c5136bc728a724b46edb78a4cc404ce1ebe8
Tuan Dat <phamtuandat.work@gmail.com>
2025-11-02 23:28:44 +0700

    cap nhat giao dien datve , chonghe

---
+++ src/gui/ChonGheGUI.java
@@ -0,0 +1,214 @@
+package gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+import dao.GheDAO;
+import dao.PhongDAO;
+import entity.Ghe;
+import entity.Phong;
+/***
+ * @author Tuan Dat
+ */
+public class ChonGheGUI extends JFrame implements ActionListener {
+
+    // Lưu danh sách ghế
+	private PhongDAO phong;
+	private Phong room;
+    private JButton[] danhSachGhe;
+    private Ghe ghe;
+	private JButton btnXacNhan;
+	private JButton btnHuy;
+	private GheDAO gheDao;
+	private ArrayList<String> maGheChon;
+	private DatVeGUI parentFrame;
+   
+    public ChonGheGUI(String maPhong,PhongDAO phong, GheDAO ghe, DatVeGUI parentFrame) {
+    	this.phong = phong;
+    	this.room = phong.findPhongByMa(maPhong);
+    	this.danhSachGhe = new JButton[room.getSoLuongGhe()];
+    	this.gheDao = ghe;
+    	this.parentFrame = parentFrame;
+    	
+    	if(this.room == null) {
+    		JOptionPane.showConfirmDialog(this, "Vui lòng chọn phòng");
+    		return;
+    	}
+    	
+        initGUI();
+        setLocationRelativeTo(null);
+    }
+
+    private void initGUI() {
+        setTitle("Chọn ghế");
+        setLayout(new BorderLayout());
+        setSize(750, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // ----- Tiêu đề -----
+        JPanel pnlTitle = new JPanel();
+        JLabel lblTitle = new JLabel("SƠ ĐỒ GHẾ");
+        lblTitle.setFont(new Font("Arial", Font.BOLD, 28));
+        lblTitle.setForeground(Color.BLUE);
+        pnlTitle.add(lblTitle);
+        add(pnlTitle, BorderLayout.NORTH);
+
+        // ----- Khu vực trung tâm -----
+        JPanel pnlCenter = new JPanel();
+        pnlCenter.setLayout(new BoxLayout(pnlCenter, BoxLayout.Y_AXIS));
+        pnlCenter.setBackground(Color.WHITE);
+
+        // Màn hình rạp 
+        ImageIcon manHinh = new ImageIcon(new ImageIcon("img/man_hinh.png").getImage().getScaledInstance(300, 50, java.awt.Image.SCALE_SMOOTH));
+        JLabel lblScreen = new JLabel(manHinh, SwingConstants.CENTER);
+        lblScreen.setFont(new Font("Arial", Font.BOLD, 20));
+        JPanel pnlScreen = colorBox(Color.BLACK, 600, 5);
+        
+        pnlScreen.setLayout(new BorderLayout());
+        pnlScreen.add(lblScreen, BorderLayout.CENTER);
+        pnlScreen.setAlignmentX(Component.CENTER_ALIGNMENT);
+        pnlCenter.add(Box.createVerticalStrut(20));
+        pnlCenter.add(pnlScreen);
+        pnlCenter.add(Box.createVerticalStrut(20));
+
+        // ----- Sơ đồ ghế -----
+        JPanel pnlGhe = createGhe();
+
+        // ----- Ghi chú màu -----
+        JPanel pnlLegend = new JPanel();
+        pnlLegend.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        pnlLegend.add(new JLabel("Ghi chú: "));
+        pnlLegend.add(colorBox(Color.LIGHT_GRAY, 20, 20));
+        pnlLegend.add(new JLabel("Ghế trống "));
+        pnlLegend.add(colorBox(Color.RED, 20, 20));
+        pnlLegend.add(new JLabel("  Đã đặt "));
+        pnlLegend.add(colorBox(Color.GREEN, 20, 20));
+        pnlLegend.add(new JLabel("  Đang chọn "));
+        
+
+        // Gộp ghế + ghi chú
+        JPanel pnlGheBox = new JPanel(new BorderLayout());
+        pnlGheBox.add(pnlGhe, BorderLayout.CENTER);
+        pnlGheBox.add(pnlLegend, BorderLayout.SOUTH);
+        
+        
+        // Nut 
+        JPanel pnlButton = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        pnlButton.add(btnXacNhan = new JButton("Xác nhận chọn"));
+        pnlButton.add(btnHuy = new JButton("Hủy"));
+        
+        btnXacNhan.addActionListener(this); 
+        btnHuy.addActionListener(this);
+        pnlCenter.add(pnlGheBox);
+       
+
+        add(pnlCenter, BorderLayout.CENTER);
+        add(pnlButton, BorderLayout.SOUTH);
+
+//        setVisible(true);
+    }
+
+    private JPanel colorBox(Color color, int w, int h) {
+        JPanel box = new JPanel();
+        box.setBackground(color);
+        box.setPreferredSize(new Dimension(w, h));
+        box.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        return box;
+    }
+    private JPanel createGhe() {
+    	int soHang = (int) Math.ceil((double) danhSachGhe.length / 10);
+        JPanel pnlGhe = new JPanel(new GridLayout(soHang, 10, 7, 7));
+        pnlGhe.setBackground(Color.WHITE);
+
+        int rowChar = 64; 
+        for (int i = 0; i < danhSachGhe.length; i++) {
+            if (i % 10 == 0) {
+                rowChar += 1;
+            }
+            int seatNumber = (i % 10) + 1;
+            String seatLabel = String.valueOf((char) rowChar) + seatNumber;
+
+            JButton ghe = new JButton(seatLabel);
+            ghe.setBackground(Color.LIGHT_GRAY);
+            ghe.setFocusPainted(false);
+            ghe.addActionListener(this);
+
+            danhSachGhe[i] = ghe;
+            pnlGhe.add(ghe);
+        }
+        
+        setGheTrong();
+        return pnlGhe;
+    }
+    private void setGheTrong() {
+        ArrayList<Ghe> listGheDat = gheDao.getAllGhe();
+
+        for (JButton btnGhe : danhSachGhe) {
+            String maGhe = btnGhe.getText();
+            boolean isDat = false;
+            
+            // kiem tra ghe duoc dat hay chua
+            for (Ghe ghe : listGheDat) {
+                if (ghe.getMaGhe().equals(maGhe)) {
+                    isDat = true;
+                    break;
+                }
+            }
+            // set mau
+            if (!isDat) {
+                btnGhe.setBackground(Color.lightGray); 
+                btnGhe.setEnabled(true);
+            } else {
+                btnGhe.setBackground(Color.RED); 
+                btnGhe.setEnabled(false);
+            }
+        }
+    }
+
+    private void setGheDaChon() {
+    	maGheChon = new ArrayList<String>();
+    	
+    	int i =0 ;
+    	for (JButton jButton : danhSachGhe) {
+			if(jButton.getBackground() == Color.green) {
+				maGheChon.add(jButton.getText());
+			}
+		}
+    }
+    
+    public ArrayList<String> getGheDaChon() {
+		return maGheChon;
+	}
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    	Object source = e.getSource();
+    	if(source.equals(btnHuy)) {
+    		this.dispose();
+    		
+    	}else if(source.equals(btnXacNhan)) {
+    		setGheDaChon();
+    		if(parentFrame != null) {
+    			parentFrame.showGhe(maGheChon);
+    		}
+    		this.dispose();
+    		
+    	}else if(source instanceof JButton) {
+    		JButton ghe = (JButton) e.getSource();
+
+    		if (ghe == btnHuy || ghe == btnXacNhan) return;
+    		
+    		if (ghe.getBackground().equals(Color.GREEN)) {
+    		    ghe.setBackground(Color.LIGHT_GRAY);
+    		} else if (ghe.getBackground().equals(Color.LIGHT_GRAY)) {
+    		    ghe.setBackground(Color.GREEN);
+    		} else if (ghe.getBackground().equals(Color.RED)) {
+    		    JOptionPane.showMessageDialog(this, "Ghế " + ghe.getText() + " đã được đặt!");
+    		}
+    	}
+        
+    }
+}
--- src/gui/DatVeGUI.java
+++ src/gui/DatVeGUI.java
@@ -10,8 +10,8 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -19,21 +19,36 @@ import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import dao.DatVeDAO;
+import dao.GheDAO;
+import dao.KhachHangDAO;
+import dao.LichChieuDAO;
 import dao.PhimDAO;
 import dao.PhongDAO;
 import entity.*;
 
 
-/***
+/**
+ * Giao diện đặt vé của hệ thống quản lý rạp chiếu phim.
  * 
- * @author Tuan Dat
+ * Lớp này cho phép người dùng:
+ * 
+ * Nhập thông tin khách hàng (họ tên, số điện thoại).
+ * Chọn phim, phòng chiếu và suất chiếu.
+ * Chọn loại ghế, ghế cụ thể và số lượng vé cần đặt.
+ * Hiển thị thông tin phim (poster, chi tiết phim,...)
+ * Xác nhận hoặc làm mới thông tin đặt vé.
+ * 
+ * Giao diện được xây dựng bằng các thành phần Swing như JPanel,  JLabel, JComboBox, JTextField, và  JButton.
+ * 
+ * @author Phạm Tuấn Đạt
+ * @version 1.0
  */
 public class DatVeGUI extends JPanel implements ActionListener{
 	private JTextField txtTenKH;
@@ -52,19 +67,24 @@ public class DatVeGUI extends JPanel implements ActionListener{
 	private JButton btnChonGhe;
 	private JLabel lblTenPhim;
 	private JLabel lblMoTa;
-	private JLabel lblNgayChieu;
+//	private JLabel lblNgayChieu;
 	private JLabel lblThoiLuong;
 	private static final int WIDTH = 1200, HEIGHT = 700;
 	private DatVeDAO datve;
 	private PhongDAO phong;
 	private PhimDAO phim;
 	private JLabel lblTheLoai;
+	private DatVe dv;
+	private LichChieuDAO lichChieu;
+
+	private JLabel lblNamXB;
 
 	
-	public DatVeGUI(DatVeDAO datve, PhongDAO phong, PhimDAO phim){
-		this.datve = datve;
-	    this.phong = phong;
-	    this.phim = phim;
+ 	public DatVeGUI(){
+		this.datve = new DatVeDAO();
+		this.phong = new PhongDAO();
+		this.phim = new PhimDAO();
+		this.lichChieu = new LichChieuDAO();
 	    setLayout(new BorderLayout());
 		initDatVe();
 	}
@@ -72,8 +92,10 @@ public class DatVeGUI extends JPanel implements ActionListener{
 	private void initDatVe() {	
 		JPanel pnlTitle = new JPanel();
 		JLabel lblTitle;
+		pnlTitle.add(Box.createVerticalStrut(10));
 		pnlTitle.add(lblTitle = new JLabel("ĐẶT VÉ", SwingConstants.CENTER));
-		lblTitle.setFont(new Font("arial", Font.BOLD, 40));
+		pnlTitle.add(Box.createVerticalStrut(10));
+		lblTitle.setFont(new Font("arial", Font.BOLD, 35));
 		lblTitle.setForeground(Color.blue);
 		
 		add(lblTitle, BorderLayout.NORTH);
@@ -107,7 +129,7 @@ public class DatVeGUI extends JPanel implements ActionListener{
 		pnlChonPhim.setLayout(new GridLayout(4, 2, 5, 5));
 		pnlChonPhim.add(new JLabel("Phim: "));pnlChonPhim.add(cboPhim= new JComboBox<String>(getDanhSachTenPhim()));
 		pnlChonPhim.add(new JLabel("Phòng:"));pnlChonPhim.add(cboPhong= new JComboBox<String>(getDanhSachTenPhong()));
-		pnlChonPhim.add(new JLabel("Lịch chiếu:"));pnlChonPhim.add(cboLichChieu= new JComboBox<String>());
+		pnlChonPhim.add(new JLabel("Lịch chiếu:"));pnlChonPhim.add(cboLichChieu= new JComboBox<String>(getDanhSachTenLichChieu()));
 		
 		
 		
@@ -115,13 +137,16 @@ public class DatVeGUI extends JPanel implements ActionListener{
 		pnlChonGhe.setBorder(BorderFactory.createTitledBorder("CHỌN LOẠI VÉ VÀ GHẾ"));
 		pnlChonGhe.setLayout(new GridLayout(5, 2, 10, 10));
 		
-		pnlChonGhe.add(new JLabel("Loại ghế: "));
+		pnlChonGhe.add(new JLabel("Loại ghế chọn: ", SwingConstants.CENTER));
 		pnlChonGhe.add(lblListLoaiGhe = new JLabel("Chưa chọn"));
-		pnlChonGhe.add(new JLabel("Ghế đã chọn:"));
+		pnlChonGhe.add(new JLabel("Ghế đã chọn:", SwingConstants.CENTER));
 		pnlChonGhe.add(lblListGhe =  new JLabel("Chưa chọn ghế"));
-		pnlChonGhe.add(new JLabel("Số lượng ghế:"));
+		pnlChonGhe.add(new JLabel("Số lượng ghế:", SwingConstants.CENTER));
 		pnlChonGhe.add(lblSLVe =  new JLabel("Chưa chọn"));
 		pnlChonGhe.add(btnChonGhe = new JButton("Chọn ghế"));
+		
+		btnChonGhe.setBackground(Color.red);
+		btnChonGhe.setForeground(Color.white);
 		
 		
 		lblListLoaiGhe.setEnabled(false);
@@ -133,6 +158,11 @@ public class DatVeGUI extends JPanel implements ActionListener{
 		
 		pnlButton.add(btnLamMoi= new JButton("Làm mới"));
 		pnlButton.add(btnXacNhanDatVe= new JButton("Xác nhận đặt vé"));
+		
+		btnLamMoi.setBackground(Color.green);
+		btnXacNhanDatVe.setBackground(Color.blue);
+		btnLamMoi.setForeground(Color.white);
+		btnXacNhanDatVe.setForeground(Color.white);
 		
 		
 		pnlCenter.add(pnlThongTinKH);
@@ -152,7 +182,7 @@ public class DatVeGUI extends JPanel implements ActionListener{
 		pnlEast.setBorder(BorderFactory.createTitledBorder("THÔNG TIN PHIM"));
 		pnlEast.setPreferredSize(new Dimension(450, pnlEast.getHeight()));
 	
-		ImageIcon icon = new ImageIcon(new ImageIcon("img/doraemon.jpg").getImage().getScaledInstance(300, 200, java.awt.Image.SCALE_SMOOTH));
+		ImageIcon icon = new ImageIcon(new ImageIcon("img/doraemon.jpg").getImage().getScaledInstance(300, 300, java.awt.Image.SCALE_SMOOTH));
 		lblPosterPhim = new JLabel(icon);
 		lblPosterPhim.setPreferredSize(new Dimension(400, 300));
 		
@@ -165,8 +195,8 @@ public class DatVeGUI extends JPanel implements ActionListener{
 		pnlThongTinPhim.add(lblTheLoai=new JLabel());
 		pnlThongTinPhim.add(new JLabel("Mô tả:")).setEnabled(false);
 		pnlThongTinPhim.add(lblMoTa=new JLabel());
-		pnlThongTinPhim.add(new JLabel("Ngày chiếu:")).setEnabled(false);
-		pnlThongTinPhim.add(lblNgayChieu=new JLabel());
+		pnlThongTinPhim.add(new JLabel("Năm xuất bản:")).setEnabled(false);
+		pnlThongTinPhim.add(lblNamXB=new JLabel());
 		pnlThongTinPhim.add(new JLabel("Thời lượng:")).setEnabled(false);
 		pnlThongTinPhim.add(lblThoiLuong = new JLabel());
 		
@@ -188,11 +218,7 @@ public class DatVeGUI extends JPanel implements ActionListener{
 	 	
 	}
 	public static void main(String[] args) {
-		DatVeDAO datve = new DatVeDAO();
-	    PhongDAO phong = new PhongDAO();
-	    PhimDAO phim = new PhimDAO();
-	    datve.connectDatabase();
-	    new DatVeGUI(datve, phong, phim);
+	    new DatVeGUI();
 		
 	}
 	@Override
@@ -201,20 +227,30 @@ public class DatVeGUI extends JPanel implements ActionListener{
 		if(source.equals(btnLamMoi)) {
 			txtTenKH.setText("");
 			txtSDT.setText("");
-			lblListGhe.setText("");
-			lblListLoaiGhe.setText("");
-			lblSLVe.setText("");
-			lblTheLoai.setText("");
+			lblListGhe.setText("Chưa chọn");
+			lblListLoaiGhe.setText("Chưa chọn");
+			lblSLVe.setText("Chưa chọn");
 			txtTenKH.requestFocus();
 			cboPhim.setSelectedIndex(0);
 			cboLichChieu.setSelectedIndex(0);
 			cboPhong.setSelectedIndex(0);
 			
 		}else if(source.equals(btnChonGhe)) {
-			
+			GheDAO ghe = new GheDAO();
+		    ChonGheGUI chonGheGUI =	new ChonGheGUI("P01", phong, ghe, this);
+		    chonGheGUI.setVisible(true);
+		    
 		}else if(source.equals(btnXacNhanDatVe)) {
-			if(datve.addDatVe()) {
-				
+			dv = new DatVe(TOOL_TIP_TEXT_KEY, TOOL_TIP_TEXT_KEY, null);
+			if(datve.addDatVe(dv)) {
+				if(saveThongThinKhachHang()) {
+					JOptionPane.showConfirmDialog(this, "Đặt vé thành công.");
+					return;
+				}
+				else {
+					JOptionPane.showConfirmDialog(this, "Đặt vé thất bại. Vui lòng kiểm tra lại.");
+					return;
+				}
 			}
 		}else if(source.equals(cboPhim)) {
 			showThongTinPhim();
@@ -222,27 +258,60 @@ public class DatVeGUI extends JPanel implements ActionListener{
 		
 	}
 	
+	private boolean saveThongThinKhachHang() {
+		KhachHangDAO khachHang = new KhachHangDAO();
+		int i = 11;
+		return khachHang.addKhachHang(new KhachHang("KH"+(char)(i++), txtTenKH.getText(), txtSDT.getText()));
+	}
+
+	
+	public void showGhe(ArrayList<String> dsGheChon) {
+		
+		String s = new String();
+		for (String string : dsGheChon) {
+			s += string +", ";
+		}
+		
+		lblListGhe.setEnabled(true);
+		lblListGhe.setText(s);
+		lblListGhe.setEnabled(false);
+		
+		lblSLVe.setEnabled(true);
+		lblSLVe.setText(String.valueOf(dsGheChon.size()));
+		lblSLVe.setEnabled(false);
+	}
+	
 	private String[] getDanhSachTenPhong() {
-		String[] phongItems = new String[phong.getSize()+1];
+		ArrayList<Phong> listP = phong.getAllPhong();
+		String[] phongItems = new String[listP.size()+1];
 		phongItems[0] = "----------------------------- Phòng ------------------------------------";
-		for(int i=0; i<phong.getSize(); i++) {
-			phongItems[i+1] = phong.findPhongByIndex(i).getTenPhong();
+		for(int i=0; i<listP.size(); i++) {
+			phongItems[i+1] = listP.get(i).getTenPhong();
 		}
 		return phongItems;
 	}
 	private String[] getDanhSachTenPhim() {
-		String[] phimItems = new String[phim.getSize()+1];
-		phimItems[0] = "----------------------------- Phim ------------------------------------";
-		for(int i=0; i<phim.getSize(); i++) {
-			phimItems[i+1] = phim.findPhimByIndex(i).getTenPhim();
+		List<Phim> lp = phim.getAll();
+		String[] phimItems = new String[lp.size()+1];
+		phimItems[0] = "------------------------------- Phim ------------------------------------";
+		for(int i=0; i<lp.size(); i++) {
+			phimItems[i+1] = lp.get(i).getTenPhim();
 		}
 		return phimItems;
+	}
+	private String[] getDanhSachTenLichChieu() {
+		ArrayList<LichChieu> lc = lichChieu.getAllLichChieu();
+		String[] lichItems = new String[lc.size()+1];
+		lichItems[0] = "--------------------------- Lịch Chiếu ---------------------------------";
+		for(int i=0; i<lc.size(); i++) {
+			lichItems[i+1] = lc.get(i).getGioBatDau().toString();
+		}
+		return lichItems;
 	}
 	
 	private void showThongTinPhim() {
 		String tenPhim =  (String) cboPhim.getSelectedItem();
 		Phim p = new Phim();
-		
 		for (int i=0;i<phim.getSize(); i++) {
 			p = phim.findPhimByTen(tenPhim);
 		}
@@ -250,8 +319,8 @@ public class DatVeGUI extends JPanel implements ActionListener{
 			lblTenPhim.setText(p.getTenPhim());
 			lblTheLoai.setText(p.getLoaiPhim().getTenLoaiPhim());
 			lblMoTa.setText(p.getMoTa());
-			lblNgayChieu.setText(String.valueOf(p.getNamPhatHanh()));
-			lblThoiLuong.setText(String.valueOf(p.getThoiLuongChieu()));
+			lblNamXB.setText(String.valueOf(p.getNamPhatHanh()));
+			lblThoiLuong.setText(String.valueOf(p.getThoiLuongChieu() + " phút"));
 		}else {
 			System.out.println("Không tìm thấy Phim");
 		}

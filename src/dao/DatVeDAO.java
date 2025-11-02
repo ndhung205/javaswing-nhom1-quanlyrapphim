94a5742d1da3778113b935a1a9cf275ff4e9f4b1
Tuan Dat <phamtuandat.work@gmail.com>
2025-11-02 20:47:56 +0700

    Update DatVeDAO.java

--- src/dao/DatVeDAO.java
+++ src/dao/DatVeDAO.java
@@ -2,13 +2,15 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 
+import connectDB.DatabaseConnection;
 import entity.DatVe;
-
+import entity.Ve;
 
 public class DatVeDAO {
 	private ArrayList<DatVe> listDatVe;
@@ -44,8 +46,23 @@ public class DatVeDAO {
 			e.printStackTrace();
 		}
 	}
-	public boolean addDatVe() {
-		return true;
+	public boolean addDatVe(DatVe dv) {
+		con = DatabaseConnection.getInstance().getConnection();
+		PreparedStatement stmt = null;
+		int n =0;
+		try {
+			stmt = con.prepareStatement("INSERT INTO DatVe VALUES (?,?,?,?,?)");
+			stmt.setString(1, dv.getMaDatVe());
+			stmt.setString(2, null);
+			stmt.setString(3, dv.getNgayDat().toLocaleString());
+			stmt.setString(4, dv.getTrangThai());
+			stmt.setString(5, null);
+			
+			n = stmt.executeUpdate();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return n>0;
 	}
 	
 	
